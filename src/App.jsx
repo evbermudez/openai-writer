6bf790ce7948abe5b9c3b5ba3577ec1fde8d4fb4
Eric Vincent Bermudez <ev.bermudez@gmail.com>
2023-09-02 20:36:50 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,50 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [input, setInput] = useState('');
+  const [textQuery, setTextQuery] = useState('');
 
-  const handleInputChange = (e) => {
-    setInput(e.target.value);
+  const handleTextQueryChange = (e) => {
+    setTextQuery(e.target.value);
+  };
+
+  const handleOpenAICall = async () => {
+    console.log('submit');
+
+    const endpointUrl = import.meta.env.VITE_OPENAPI_ENDPOINT_URL;
+    const apiKey = import.meta.env.VITE_OPENAPI_API_KEY;
+
+    // The body of your request (e.g., a prompt for GPT-3)
+    const requestBody = {
+      prompt: textQuery,
+      max_tokens: 60
+    };
+
+    try {
+      const response = await fetch(endpointUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify(requestBody)
+      });
+
+      const data = await response.json();
+      res.json(data);
+    } catch (error) {
+      console.error("Error calling OpenAPI:", error);
+      res.status(500).json({ error: "Failed to call OpenAPI" });
+    }
   };
 
   return (
     <div>
       <textarea 
-        value={input} 
-        onChange={handleInputChange} 
-        placeholder="Enter your requirements here..."
+        value={textQuery} 
+        onChange={handleTextQueryChange} 
+        placeholder="Enter instructions here..."
       />
-      <button onClick={() => {/*handle the OpenAI API call */}}>
+      <button onClick={() => {handleOpenAICall}}>
         Submit
       </button>
     </div>
